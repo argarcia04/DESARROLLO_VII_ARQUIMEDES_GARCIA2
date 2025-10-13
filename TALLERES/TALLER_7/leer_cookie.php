089ca485ef609a63d09396837868024bfaf5fd84
Arquimedes Garcia Lorenzo <garquimedes2@gmail.com>
2025-10-13 18:35:31 -0500

    TALLER_7 - Paso 1: Implementación básica de cookies

---
+++ TALLERES/TALLER_7/leer_cookie.php
@@ -0,0 +1,9 @@
+
+<?php
+if(isset($_COOKIE['usuario'])) {
+    echo "Bienvenido, " . htmlspecialchars($_COOKIE['usuario']) . "!";
+} else {
+    echo "No se ha encontrado la cookie 'usuario'.";
+}
+?>
+        
